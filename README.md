d5b306bb2ebdf76ed3fab19121ec766129daf100
aamaaliaa <priliantiniamalia@gmail.com>
2024-05-30 22:12:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crud_Api_Amalia_Priliantini
+ CRUD PPB 2
---
+++ app/src/main/java/com/example/crud_api_amalia_priliantini/UpdateAddActivity.kt
@@ -0,0 +1,12 @@
+package com.example.crud_api_amalia_priliantini
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crud_api_ari_setiawan.R
+
+class UpdateAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_add)
+    }
+}
